6150ab8457e07112a8cfe0aef200080b32da7717
ps-spb <115991606+ps-spb@users.noreply.github.com>
2023-04-26 09:28:24 +0100

    Create startup.sh
    added startup.sh to be used as the entrypoint

---
+++ startup.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+DEPOT=/depot
+PKCS11CONF=${DEPOT}/pkcs11-config.json
+REGION=eu-west-1
+
+# things to grab from environment variables
+# see https://github.com/PortSwigger/scep-pkcs11/blob/main/cmd/scepserver/scepserver.go#L47 for more details
+# SCEP_CA_PASS
+# SCEP_FILE_DEPOT
+# SCEP_HTTP_LISTEN_PORT
+# SCEP_CERT_RENEW
+# SCEP_CHALLENGE_PASSWORD
+# SCEP_PKCS11_CONFIGFILE
+# SCEP_LOG_JSON (bool)
+# SCEP_LOG_DEBUG (bool)
+# 
+# things that should exist in secrets manager and be available to us via env variables
+#SM_PKCS11_CONF=""     # an ARN of the config used by scepserver -pkcs11-config argument
+#SM_KMS_CONFIG=""      # an ARN of the config file used by the pkcs11 shim to be stored in /etc/aws-kms-pkcs11/config.json
+
+# expose as ENV var
+#SM_CMD_SECRETS_ARN=""
+
+# takes secret arn, secret name. returns just the secret.
+getsecretvalue() {
+        aws secretsmanager get-secret-value --secret-id $1 --region=${REGION} | jq --raw-output '.SecretString' | jq -r .$2 
+}
+
+# takes secretarn, filename to write contents to
+getsecretblob() {
+        aws secretsmanager get-secret-value --secret-id $1 --region ${REGION} --query SecretBinary  | sed s/\"//g | base64 -d > $2
+        echo grabbing $1 saving to $2
+}
+
+# First up, check to see if our pkcs11 config file exists, and grab it if not.
+if [ ! -f ${PKCS11CONF} ]; then
+        getsecretblob ${SM_PKCS11_CONF} ${PKCS11CONF}
+fi
+
+# next, we need to grab our aws-kms-pkcs11 config.
+# this test is redundant but makes it look pretty :-)
+if [ ! -f /etc/aws-kms-pkcs11/config.json ]; then
+        mkdir -p /etc/aws-kms-pkcs11/
+        getsecretblob ${SM_KMS_CONFIG} /etc/aws-kms-pkcs11/config.json
+fi
+
+CAPASS=`getsecretvalue ${SM_CMD_SECRETS_ARN} SCEP_CA_PASS`
+CHALLENGE=`getsecretvalue ${SM_CMD_SECRETS_ARN} SCEP_CHALLENGE_PASSWORD`
+
+# at this point we have the aws-kms-pkcs11 config in place, and our pkcs11-config.json in place.
+# However we might have never ran before, so assuming so let's check to see if we have a ca.key
+# on the block storage.
+if [ ! -f ${DEPOT}/ca.key ]; then
+        # init ca
+        echo "Seems like first bootup - creating on disk CA"
+        /usr/bin/scepserver ca -init -pkcs11-config ${PKCS11CONF} -depot ${DEPOT} --key-password $CAPASS || exit 1
+fi
+
+# we should be able to start now.
+echo "attempting to start server"
+/usr/bin/scepserver -capass $CAPASS -challenge $CHALLENGE -depot $DEPOT
